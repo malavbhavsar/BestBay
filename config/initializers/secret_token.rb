735d58b3ae7f4bff75a5e359594e8e979b76d75e
Todd Sedano <todd.sedano@sv.cmu.edu>
2012-09-24 18:54:52 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestBay::Application.config.secret_token = 'b2ed1f91edd719c4a449fb2bbe2486f38f177f256e48deaa0bac185f4401ee05960a96a329cca297115fafa7f61301cdafd671489a3eec9f78804d33de448487'
