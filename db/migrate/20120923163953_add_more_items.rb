f60af9a6a3dbad105da0b96579902462d386ffa1
Frank Hsueh <frankpapa@gmail.com>
2012-09-26 15:15:42 -0400

    New pictures

--- db/migrate/20120923163953_add_more_items.rb
+++ db/migrate/20120923163953_add_more_items.rb
@@ -1,12 +1,12 @@
 class AddMoreItems < ActiveRecord::Migration
     MORE_ITEMS = [
-    { :name=>'snow white',           :picture=> 'https://ssl.gstatic.com/movies/tbn_a7d75269b295f037.jpg?size=80x107&web', :description=> 'it is about snow white', :opening_bid=> 10, :highest_bid=>20},
-    { :name=>'dredd',                :picture=> 'http://ia.media-imdb.com/images/M/MV5BODkyNDQzMzUzOF5BMl5BanBnXkFtZTcwODYyMDEyOA@@._V1._SY317_.jpg', :description=> 'it is about dredd', :opening_bid => 15, :highest_bid=>30},
-    { :name=>'the wise little girl', :picture=> 'http://ingilizce.masaldiyari.net/wp-content/uploads/2010/09/the_wise_little_girl.jpg', :description=> 'it is about little wise girl', :opening_bid=> 5, :highest_bid=>29},
-    { :name=>'The Wolf and the Seven Young Kids', :picture=> 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Wolf_and_7_kids.jpg/220px-Wolf_and_7_kids.jpg', :description=> 'it is about wolf', :opening_bid=> 10, :highest_bid=>20},
-    { :name=>'The Wounded Lion', :picture=> 'http://etc.usf.edu/clipart/59400/59410/59410_lion_wound_lg.gif', :description=> 'it is about lion', :opening_bid=> 10, :highest_bid=>20},
-    { :name=>'The Witch', :picture=> 'http://upload.wikimedia.org/wikipedia/en/thumb/4/41/TheWitches.jpg/200px-TheWitches.jpg', :description=> 'it is about witch', :opening_bid=> 10, :highest_bid=>20},
-    { :name=>'The White Duck', :picture=> 'http://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Ivan_Bilibin_011.jpg/300px-Ivan_Bilibin_011.jpg', :description=> 'it is about snow white', :opening_bid=> 10, :highest_bid=>20}
+    { :name=>'snow white',           :picture=> 'http://t0.gstatic.com/images?q=tbn:ANd9GcTWfbqckrZzXH0F-worz2xtS9E_Pxitp_RaYfSLJXqoaLzFORAh', :description=> 'it is about snow white', :opening_bid=> 10, :highest_bid=>20},
+    { :name=>'dredd',                :picture=> 'http://t1.gstatic.com/images?q=tbn:ANd9GcTIHK7A2c-thSBhgnqd9HXxMy0FUv0-gGRK2URW-ISPz5LEj2wHqg', :description=> 'it is about dredd', :opening_bid => 15, :highest_bid=>30},
+    { :name=>'the wise little girl', :picture=> 'http://t3.gstatic.com/images?q=tbn:ANd9GcTgiOkGmDCLJweBI3C_pNWY-XZThMYqjVgjE0c6w02eJov1jczVhw', :description=> 'it is about little wise girl', :opening_bid=> 5, :highest_bid=>29},
+    { :name=>'The Wolf and the Seven Young Kids', :picture=> 'http://www.gutenberg.org/files/15659/15659-h/images/illus-021.jpg', :description=> 'it is about wolf', :opening_bid=> 10, :highest_bid=>20},
+    { :name=>'The Wounded Lion', :picture=> 'http://t0.gstatic.com/images?q=tbn:ANd9GcRQ3PPIzHBNMk5dEGTsn_xrsUGdKn7jxpenx47rHlj_3IQjYRxS3A', :description=> 'it is about lion', :opening_bid=> 10, :highest_bid=>20},
+    { :name=>'The Witch', :picture=> 'http://t2.gstatic.com/images?q=tbn:ANd9GcSM2CKtww-zvmURsZxesTxSZcrNpU3YILgAiSToDelzgYXXtaD_', :description=> 'it is about witch', :opening_bid=> 10, :highest_bid=>20},
+    { :name=>'The White Duck', :picture=> 'http://t3.gstatic.com/images?q=tbn:ANd9GcRvBeTMF8PsXC8PRmAwnW7QteEHGUlVvDsu8cLW6cuZGtcKNP_WfQ', :description=> 'it is about snow white', :opening_bid=> 10, :highest_bid=>20}
     ]
 
     def up
