67adba4bc244348a08042d39d3e18d53d57c4c31
malavbhavsar <malav.bhavsar@gmail.com>
2012-09-27 17:46:51 -0400

    MOAR tests

--- spec/controllers/item_controller_spec.rb
+++ spec/controllers/item_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe ItemsController do
+  before :each do
+    @i1 = FactoryGirl.build(:item, :id=>1, :name => 'A', :description => "A is A", :highest_bid => 10)
+    @i2 = FactoryGirl.build(:item, :id=>2, :name => 'B', :description => "B is B", :highest_bid => 10)
+    @fake_results = [@i1, @i2]
+  end
   describe 'show all the items' do
-    before :each do
-      @i1 = FactoryGirl.build(:item, :id=>1, :name => 'A', :description => "A is A", :highest_bid => 10)
-      @i2 = FactoryGirl.build(:item, :id=>2, :name => 'B', :description => "B is B", :highest_bid => 10)
-      @fake_results = [@i1, @i2]
-    end
     it 'model should receive the call to method' do
       Item.should_receive(:all)
       get :index
@@ -18,6 +18,21 @@ describe ItemsController do
       end
       it 'it should assign the values to the variable in view' do
         assigns(:items).should == @fake_results
+      end
+    end
+  end
+  describe 'go to one particular item' do
+    it 'model should receive the call to find and return appropriate value' do
+      Item.should_receive(:find).with(@i1.id.to_s).and_return(@i1)
+      get :show, :id=>@i1.id
+    end
+    describe 'testing the show view' do
+      before :each do
+        Item.stub(:find).with(@i1.id.to_s).and_return(@i1)
+        get :show, :id=>@i1.id
+      end
+      it 'it should assign the value to the variable in the view' do
+        assigns(:item).should == @i1
       end
     end
   end
