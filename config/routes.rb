61a5ff92f1bf301091f856b362750e7896e2105a
malavbhavsar <malav.bhavsar@gmail.com>
2012-09-28 16:40:27 -0400

    Adding devise stuff...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 BestBay::Application.routes.draw do
+  devise_for :users
+
   resources :items
 
+  root to:'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
