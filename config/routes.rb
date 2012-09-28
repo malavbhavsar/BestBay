758d79024524b3e90e56ac0c8db403bc0980c285
Frank Hsueh <frankpapa@gmail.com>
2012-09-28 16:11:12 -0400

    Page route initail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 BestBay::Application.routes.draw do
   resources :items
 
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
