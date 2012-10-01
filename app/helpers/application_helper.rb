64da01cb8f1eac1228cf7eade9bcd4fa7d1c159a
malavbhavsar <malav.bhavsar@gmail.com>
2012-10-01 01:50:28 -0400

    Refactoring views and making two devise views use bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :success then "success"
+      when :info then "info"
+      when :notice then "info"
+      when :error then "error"
+      when :alert then "info"
+      else "block"
+    end
+  end
 end
