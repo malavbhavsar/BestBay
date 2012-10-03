41e1ec25219dc5bea922a1f9d38a66f9a299ef48
malavbhavsar <malav.bhavsar@gmail.com>
2012-10-03 13:33:21 -0400

    minor changes in static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ BestBay::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match '/help',    to: 'static_pages#help',   as: 'help'
+  match '/about',   to: 'static_pages#about',  as: 'about'
+  match '/contact', to: 'static_pages#contact',as: 'contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
