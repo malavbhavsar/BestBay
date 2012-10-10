d6b515d51f9b7c10c0db304647d11ebe7f0bc0f8
Harshad Mande <Harshad.Mande@gmail.com>
2012-10-09 22:20:39 -0400

    place bid

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ BestBay::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  #  match 'items/:id/bid' => 'items#place_bid'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
