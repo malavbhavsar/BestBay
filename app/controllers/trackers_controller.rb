f794b4f7b3b969933208a63da37c2528d0a3bcd3
malavbhavsar <malav.bhavsar@gmail.com>
2012-11-20 12:28:01 -0500

    PG needs oen to add id in group

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -5,8 +5,8 @@ class TrackersController < ApplicationController
   # GET /tracker.json
   def show
     @tracker_won = current_user.bids.find_all_by_tracked(true)
-    @tracker_bids = current_user.bids.select("id, item_id, user_id, max(amount) as amount").group("item_id")
-    @tracker_auctions = Bid.where('item_id in (?)', current_user.items).select("id, item_id, user_id, max(amount) as amount").group("item_id")
+    @tracker_bids = current_user.bids.select("id, item_id, user_id, max(amount) as amount").group("id, item_id")
+    @tracker_auctions = Bid.where('item_id in (?)', current_user.items).select("id, item_id, user_id, max(amount) as amount").group("id, item_id")
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: [@tracker_won, @tracker_auctions, @tracker_bids] }
